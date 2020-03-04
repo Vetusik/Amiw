c666307549a2267c7d7221c3f6557b356319badb
Vetusik <44228955+Vetusik@users.noreply.github.com>
2020-03-04 16:25:06 +0100

    Homepage finished

--- src/app/nawigacja/nawigacja.component.ts
+++ src/app/nawigacja/nawigacja.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nawigacja',
   templateUrl: './nawigacja.component.html',
-  styleUrls: ['./nawigacja.component.scss']
+  styleUrls: ['./nawigacja.component.scss', '../app.component.scss']
 })
 export class NawigacjaComponent implements OnInit {
 
